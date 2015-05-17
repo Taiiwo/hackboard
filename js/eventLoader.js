1153e7df22fee7aa7554bfc3be6174b4676aba5c
Taiiwo Llort <tylerzacc@gmail.com>
2015-05-17 11:23:32 +0100

    add event loader lib

---
+++ js/eventLoader.js
@@ -0,0 +1,15 @@
+var eventData;
+function EventLoader(){
+  this.search = function(term){
+    $.getJSON(
+      "https://www.eventbriteapi.com/v3/events/search/",
+      {
+        q: term,
+        token: "R5KWMVH7GMWKCTBS6CQH"
+      },
+      function(data, status){
+        eventData = data;
+      }
+    );
+  }
+}
