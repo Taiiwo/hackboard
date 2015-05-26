8c973b1ab7fbd33286bb2fd7ca7ab749ad29a74d
Taiiwo Llort <tylerzacc@gmail.com>
2015-05-26 17:32:19 +0100

    event urls

--- js/eventLoader.js
+++ js/eventLoader.js
@@ -12,9 +12,17 @@ function EventLoader(){
         this.events = data;
         this.status = status;
         $( document ).trigger("eventChanged", this.events, this.status);
+      }
+    );
+  }
+  this.searchID = function(id){
+    $.getJSON(
+      "https://www.eventbriteapi.com/v3/events/" + id + '/',
+      {
+        token: "R5KWMVH7GMWKCTBS6CQH"
       },
-      function (){
-        this.events = data;
+      function(data, status){
+        this.events = {events : [data]};
         this.status = status;
         $( document ).trigger("eventChanged", this.events, this.status);
       }
