00f5220a12a38c0c237359403b13fc39f7e31538
Taiiwo Llort <tylerzacc@gmail.com>
2015-06-02 13:53:14 +0100

    Remove the possibility of requesting a duplicate search

--- js/githubWrapper.js
+++ js/githubWrapper.js
@@ -1,11 +1,15 @@
 function GithubWrapper(){
   this.baseUrl = "https://api.github.com/";
+  this._searchHistory = [];
+  var self = this;
   this.searchRepo = function(term, callback){
     var c = "search/repositories";
     var q = {
       q: term
     }
-    this._query(c, q, callback);
+    if (this._notRedundant(c, q, callback)){
+      this._query(c, q, callback);
+    }
   }
   this._query = function(c, q, callback){
     $.getJSON(
@@ -13,8 +17,19 @@ function GithubWrapper(){
       q,
       function(data){
         callback(data);
+        self._searchHistory.push([c, q, data])
       }
     );
+  }
+  this._notRedundant = function(c, q, callback){
+    for (var i in this._searchHistory){
+      var search = this._searchHistory[i];
+      if (search[0] == c && JSON.stringify(search[1]) == JSON.stringify(q)){
+        callback(search[2]);
+        return false;
+      }
+    }
+    return true;
   }
 }
 window.githubApi = new GithubWrapper;
